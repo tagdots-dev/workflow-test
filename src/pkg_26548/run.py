d7a105554c54d467610a7a2001fa8ef658826f23
Andre Choi <andreclc@gmail.com>
2025-08-10 23:30:21 -0700

    resolve core_limit_usage_estimate UnboundLocalError

--- src/pkg_26548/run.py
+++ src/pkg_26548/run.py
@@ -479,7 +479,6 @@ def main(dry_run, repo_url, min_runs, max_days):
         if check_user_inputs(repo, repo_url, min_runs, max_days):
             delete_orphan_workflow_runs_count = 0
             delete_active_workflow_runs_count = 0
-            core_limit_usage_estimate = None
 
             """
             get all workflow runs
