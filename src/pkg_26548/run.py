0aafbc43aa3f446ee2c7ab2cbb2a95fdc1e9eb7e
Andre Choi <andreclc@gmail.com>
2025-08-13 17:32:00 -0700

    fix cron-tasks

--- src/pkg_26548/run.py
+++ src/pkg_26548/run.py
@@ -603,4 +603,4 @@ def main(dry_run, repo_url, min_runs, max_days):
 
 if __name__ == '__main__':  # pragma: no cover
     data_dict = main(standalone_mode=False)
-    # print(data_dict)
+    print(data_dict)
